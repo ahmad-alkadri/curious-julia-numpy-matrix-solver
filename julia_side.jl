3ba9750759dedd492d6e49e0c9cf94b00b7c5146
ahmad-alkadri <ahmad.alkadri@outlook.com>
2023-10-11 08:19:02 +0200

    Add garbage collections

--- julia_side.jl
+++ julia_side.jl
@@ -9,16 +9,19 @@ function solve_equation(A, B)
 end
 
 for matrix_size in matrix_sizes
-    # Load matrices
-    matrix_A = Matrix(CSV.File("matrix_A_$(matrix_size).txt"; header=false) |> DataFrame)
-    
-    df_B = CSV.File("vector_B_$(matrix_size).txt"; header=false) |> DataFrame
-    vector_B = df_B[!, 1]  # Extract the first column without copying
+	# Use let block to limit the scope of variables and help with memory management
+	let matrix_A = Matrix(CSV.File("matrix_A_$(matrix_size).txt"; header = false) |> DataFrame),
+		df_B = CSV.File("vector_B_$(matrix_size).txt"; header = false) |> DataFrame,
+		vector_B = df_B[!, 1]  # Extract the first column without copying
 
-    # Run the function and benchmark
-    bm = @benchmark solve_equation($matrix_A, $vector_B)
-    
-    push!(results, (matrix_size, mean(bm).time / 1e9, mean(bm).memory / 1e6))
+		# Run the function and benchmark
+		bm = @benchmark solve_equation($matrix_A, $vector_B)
+
+		push!(results, (matrix_size, mean(bm).time / 1e9, mean(bm).memory / 1e6))
+
+		# Explicitly call the garbage collector
+		GC.gc()
+	end
 end
 
 # Save results to CSV
